082830ee35e989e7b771b7cec3ecad960934b786
sh4d0wy <sakshambhugra8@gmail.com>
2024-01-21 20:35:08 +0530

    chore: fixed bugs in login page

--- controller/loginController.js
+++ controller/loginController.js
@@ -3,13 +3,14 @@ const user = require("../model/user");
 const loginPostController = async (req, res) => {
   const givenName = req.body.username;
   const givenPassword = req.body.password;
-  
+
   const foundUser = await user.find({ username: givenName });
-  if (foundUser) {
+  if (foundUser.length>0) {
+    console.log(foundUser);
     if (givenPassword === foundUser[0].password) {
-      res.json({ message: "authenticated" }).status(200);
+      res.status(200).json({ message: "authenticated" });
     } else {
-      res.json({ message: "not authenticated" }).status(401);
+      res.status(401).json({ message: "not authenticated" });
     }
   } else {
     res.json({ message: "No user found" });
